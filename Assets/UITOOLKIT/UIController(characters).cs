14af7cd528b71ec863a62764824d55a388ebd14f
sarena <sareena.aul@student.mahidol.edu>
2025-03-19 04:05:53 +0700

    creating character selection scene

--- Assets/UITOOLKIT/UIController(characters).cs
+++ Assets/UITOOLKIT/UIController(characters).cs
@@ -1,55 +1,58 @@
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
 
 public class CharacterSelection : MonoBehaviour
 {
-    // public GameObject[] characters; 
-    public Button character1Button;
-    public Button character2Button;
-    public Button character3Button;
+
     public Button selectButton;
     public Button backButton;
 
-    private int selectedCharacterIndex = 0;
+    public AudioSource backgroundSound;
+    public AudioSource HoverSound;
 
     void Start()
     {
-        // character1Button.onClick.AddListener(() => SelectCharacter(0));
-        // character2Button.onClick.AddListener(() => SelectCharacter(1));
-        // character3Button.onClick.AddListener(() => SelectCharacter(2));
-        selectButton.onClick.AddListener(ConfirmSelection);
-        backButton.onClick.AddListener(BackToMainMenu);
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        selectButton = root.Q<Button>("selectOne-button");
+        backButton = root.Q<Button>("home-button");
 
-        // UpdateCharacterDisplay();
+        selectButton.clicked += selectButtonPressed;
+        backButton.clicked += homeButtonPressed;
+
+        AddHoverSound(selectButton);
+        AddHoverSound(backButton);  
     }
 
-    void SelectCharacter(int index)
+    void AddHoverSound(VisualElement button)
     {
-        selectedCharacterIndex = index;
-        Debug.Log("Selected Character: " + (index + 1));
-        // UpdateCharacterDisplay(); 
+        button.RegisterCallback<PointerEnterEvent>(evt => HoverSoundOn());
     }
 
-    // void UpdateCharacterDisplay()
-    // {
-    //     for (int i = 0; i < characters.Length; i++)
-    //     {
-    //         characters[i].SetActive(i == selectedCharacterIndex);
-    //     }
-    // }
-
-    void ConfirmSelection()
+    void selectButtonPressed()
     {
-        // PlayerPrefs.SetInt("SelectedCharacter", selectedCharacterIndex);
-        // PlayerPrefs.Save();
-        // Debug.Log("Character " + (selectedCharacterIndex + 1) + " Selected!");
-        // Optionally load the game scene
         SceneManager.LoadScene("LevelSelectionScene");
     }
 
-    void BackToMainMenu()
+    void homeButtonPressed()
     {
         SceneManager.LoadScene("MenuScene");
+    }
+
+
+    void HoverSoundOn(float startTime = 0f)
+    {
+    if (HoverSound != null)
+    {
+        if (startTime > 0f && startTime < HoverSound.clip.length)
+        {
+            HoverSound.time = startTime;
+        }
+        HoverSound.Play();
+    }
+    else
+    {
+        Debug.LogError("Hover sound is not assigned!");
+    }
     }
 }
