0d97f57fb14ee863b614322c3dadc4853afe714e
sarena <sareena.aul@student.mahidol.edu>
2025-03-19 02:33:41 +0700

    done levelselection

---
+++ Assets/UITOOLKIT/UIControllerLevelSelection.cs
@@ -0,0 +1,111 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+public class UIControllerLevelSelection : MonoBehaviour
+{
+    public Button levelOneButton;
+    public Button levelTwoButton;
+    public Button levelThreeButton;
+    public Button SoundOnButton;
+    public Button SoundOffButton;
+    public Button HomeButton;
+    public Button BackButton;
+
+    public AudioSource backgroundSound;
+    public AudioSource HoverSound;
+    void Start()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        levelOneButton = root.Q<Button>("levelOne-button");
+        levelTwoButton = root.Q<Button>("levelTwo-button");
+        levelThreeButton = root.Q<Button>("levelThree-button");
+
+        HomeButton = root.Q<Button>("home-button");
+        BackButton = root.Q<Button>("back-button");
+
+        SoundOnButton = root.Q<Button>("soundOn-button");
+        SoundOffButton = root.Q<Button>("soundOff-button");
+
+
+        levelOneButton.clicked += levelOneButtonPressed;
+        levelTwoButton.clicked += levelTwoButtonPressed;
+        levelThreeButton.clicked += levelThreeButtonPressed;
+
+        HomeButton.clicked += homeButtonPressed;
+        BackButton.clicked += backButtonPressed;
+
+        SoundOnButton.clicked += SoundOnButtonPressed;
+        SoundOffButton.clicked += SoundOffButtonPressed;
+
+        AddHoverSound(levelOneButton);
+        AddHoverSound(levelTwoButton);
+        AddHoverSound(levelThreeButton);
+        AddHoverSound(HomeButton);
+        AddHoverSound(BackButton);
+        AddHoverSound(SoundOnButton);
+        AddHoverSound(SoundOffButton);
+    }
+
+    void AddHoverSound(VisualElement button)
+    {
+        button.RegisterCallback<PointerEnterEvent>(evt => HoverSoundOn());
+    }
+
+    void levelOneButtonPressed() 
+    {
+        SceneManager.LoadScene("LevelOne");
+    }
+
+    void levelTwoButtonPressed() 
+    {
+        SceneManager.LoadScene("LevelTwo");
+    }
+
+    void levelThreeButtonPressed() 
+    {
+        SceneManager.LoadScene("LevelThree");
+    }
+
+    void homeButtonPressed() 
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    void backButtonPressed() 
+    {
+        SceneManager.LoadScene("CharacterSelection");
+    }
+
+    void SoundOnButtonPressed()
+    {
+        if (backgroundSound != null && !backgroundSound.isPlaying)
+        {
+            backgroundSound.Play();
+        }
+    }
+
+    void SoundOffButtonPressed()
+    {
+        if (backgroundSound != null && backgroundSound.isPlaying)
+        {
+            backgroundSound.Stop();
+        }
+    }
+
+    void HoverSoundOn(float startTime = 0f)
+    {
+    if (HoverSound != null)
+    {
+        if (startTime > 0f && startTime < HoverSound.clip.length)
+        {
+            HoverSound.time = startTime;
+        }
+        HoverSound.Play();
+    }
+    else
+    {
+        Debug.LogError("Hover sound is not assigned!");
+    }
+    }
+}
---
+++ Assets/UITOOLKIT/UIControllerMenu.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+    public Button playButton;
+    public Button guideButton;
+    public Button quitButton;
+    public Button IncreaseVolButton;
+    public Button DecreaseVolButton;
+
+    public AudioSource backgroundSound;
+    public AudioSource buttonSound;
+    void Start()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        playButton = root.Q<Button>("play-button");
+        guideButton = root.Q<Button>("guide-button");
+        quitButton = root.Q<Button>("quit-button");
+        IncreaseVolButton = root.Q<Button>("increaseVol-button");
+        DecreaseVolButton = root.Q<Button>("decreaseVol-button");
+
+        playButton.clicked += playButtonPressed;
+        guideButton.clicked += guideButtonPressed;
+        quitButton.clicked += quitButtonPressed;
+        IncreaseVolButton.clicked += IncreaseVolume;
+        DecreaseVolButton.clicked += DecreaseVolume;
+
+        AddHoverSound(playButton);
+        AddHoverSound(guideButton);
+        AddHoverSound(quitButton);
+        AddHoverSound(IncreaseVolButton);
+        AddHoverSound(DecreaseVolButton);
+    }
+
+    void AddHoverSound(VisualElement button)
+    {
+        button.RegisterCallback<PointerEnterEvent>(evt => PlayButtonSound(0.2f));
+    }
+
+    void playButtonPressed() 
+    {
+        SceneManager.LoadScene("CharacterSelection");
+    }
+
+    void guideButtonPressed() 
+    {
+        SceneManager.LoadScene("HowToPlay");
+    }
+
+    void quitButtonPressed() 
+    {
+        Application.Quit();
+    }
+
+    void IncreaseVolume()
+    {
+        if (backgroundSound != null && !backgroundSound.isPlaying)
+        {
+            backgroundSound.Play();
+        }
+    }
+
+    void DecreaseVolume()
+    {
+        if (backgroundSound != null && backgroundSound.isPlaying)
+        {
+            backgroundSound.Stop();
+        }
+    }
+
+    void PlayButtonSound(float startTime = 0f)
+    {
+    if (buttonSound != null)
+    {
+        if (startTime > 0f && startTime < buttonSound.clip.length)
+        {
+            buttonSound.time = startTime;
+        }
+        buttonSound.Play();
+    }
+    else
+    {
+        Debug.LogError("Button sound is not assigned!");
+    }
+}
+}
